00cebfaff734fe9d5fc8f19a263c6d93c552a99e
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2021-09-04 12:00:59 +0530

    Updated

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const hme = document.querySelector(".home");
 const btn_hme = document.querySelector(".btn-home");
 const input = document.querySelector(".input");
-const settings = document.querySelector(".settings");
-const output = document.querySelector(".output");
 const txtarea = document.querySelector(".textarea");
-const btn_home = document.querySelector(".btn-home");
+const btn_next = document.querySelector(".btn-next");
+const settings = document.querySelector(".settings");
 const btn_back = document.querySelector(".btn-back");
+const output = document.querySelector(".output");
 const btn_back2 = document.querySelector(".btn-back2");
+
 const deviceWidth = window.matchMedia("(max-width:991px)");
 
 function encode() {
-    if(deviceWidth.matches) {
+    if (deviceWidth.matches) {
         page1_mob();
     }
     else {
         page1();
-    }    
+    }
 }
 
+// function decode() {
+//     if(deviceWidth.matches) {
+//         // const inp_head = document.querySelector(".imput-head");
+//         // inp_head.innerHTML = "Cypher Text";
+//         page1_mob();
+//     }
+//     else {
+//         page1();
+//         const inp_head = document.querySelector(".imput-head");
+//         inp_head.innerHTML = "Cypher Text";
+//     }    
+// }
+
 function home() {
-    if(deviceWidth.matches) {
     hme.classList.remove("d-none");
     hme.classList.add("d-flex");
-    input.classList.add("d-flex");
-    settings.classList.add("d-flex");
-    output.classList.add("d-flex");
+    input.classList.add("d-none");
+    settings.classList.add("d-none");
+    output.classList.add("d-none");
 }
 
 function page1() {
     hme.classList.remove("d-flex");
     hme.classList.add("d-none");
+    btn_hme.classList.remove("d-none");
+    btn_hme.classList.add("d-flex");
+    btn_next.classList.add("d-none");
+    btn_next.classList.remove("d-flex");
+    btn_hme.classList.add("d-flex");
     input.classList.remove("d-none");
     input.classList.add("d-flex");
     settings.classList.remove("d-none");
@@ -47,8 +65,8 @@ function page1_mob() {
     settings.classList.add("d-none");
     output.classList.remove("d-flex");
     output.classList.add("d-none");
-    btn_home.classList.add("d-flex");
-    btn_home.classList.remove("d-none");
+    btn_hme.classList.add("d-flex");
+    btn_hme.classList.remove("d-none");
     btn_back.classList.remove("d-none");
     btn_back.classList.add("d-flex");
     btn_back2.classList.remove("d-none");
